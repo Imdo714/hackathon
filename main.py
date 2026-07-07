96556ac1cc07f1b512f88553b4ea7eef6f914327
Imdo714 <gusehdla@naver.com>
2026-07-07 22:43:32 +0900

    feat: vercel 환경에 맞게 배포 설정

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
+from project.app import app  # Vercel Serverless Function이 이 'app' 객체를 찾아 실행합니다.
 
 if __name__ == "__main__":
     print("🚀 FastAPI 메인 서버를 시작합니다...")
-    # project 폴더 내의 app.py 파일에 정의된 app(FastAPI 인스턴스)을 실행합니다.
+    # 로컬 테스트용 실행 코드
     uvicorn.run("project.app:app", host="0.0.0.0", port=8000, reload=True)
